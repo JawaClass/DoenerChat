2c43b631ffedcc87d3ab07de2f359a5a918116c5
Fabian Grünwald <fabiangruenwald1995@googlemail.com>
2026-01-11 12:09:32 +0000

    spring boot devcontainer init

---
+++ backend/src/main/java/com/doener/doener/DoenerApplication.java
@@ -0,0 +1,13 @@
+package com.doener.doener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoenerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/doener/doener/DoenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doener.doener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
